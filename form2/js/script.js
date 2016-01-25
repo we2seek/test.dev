1c2b147a37fb8aae5deff32a4c71c034276930ce
Vitaliy Timchenko (lenovo) <vitaliy.timchenko@gmail.com>
2016-01-25 15:54:28 +0200

    Make radiobuttons correctly work. Customize jQuery validation plugin

--- form2/js/script.js
+++ form2/js/script.js
@@ -10,7 +10,8 @@ var dl = $('#mform .right > dl');
 var radioProd = $('input[name="product"]');
 var radioProdType = $('input[name="productType"]');
 
-var step = 0; //current step
+var step = 0; // current step
+var nextStep = 0;
 var stepStack = [];
 var max_step = fieldsets.length - 1;
 
@@ -18,51 +19,35 @@ $(document).ready(function () {
     btnPrev.prop('disabled', true);
 
     btnNext.click(function () {
-        showStep(step + 1);
+        if (
+            step == 0 &&
+            radioProdType.filter(':checked').length > 0 &&
+            radioProd.filter(':checked').length == 0
+        ) {
+            showStep(step + 2)
+        } else {
+            showStep(step + 1);
+        }
     });
 
     btnPrev.click(function () {
         showStep(stepStack.pop());
     });
 
-    form.validate({
-        rules: {
-            inputStep3: {
-                required: true,
-                minlength: 6
-            },
-            productType: {
-                required: function() {
-                    return $('input[name="productType"]:checked').val() == 'true';
-                },
-            },
-        },
-        errorPlacement: function(error, element) {
-            if ( element.is(":radio") ) {
-                error.appendTo( element.parents('.container') );
-            } else { // This is the default behavior 
-                error.insertAfter( element );
-            }
-        },
-        messages: {
-            productType: {
-                required: "Make the choice, mother fucker!",
-            }
-        },
-    });
-    
-    radioProd.click(function(){
-        showStep(step + 1);
+    radioProd.click(function () {
         // Deselect productType checked if there
         radioProdType.filter(':checked').prop('checked', false);
+
+        showStep(step + 1);
     });
-    
-    radioProdType.click(function(){
+
+    radioProdType.click(function () {
         // Escape step with prodType selection
-        if (step == 0){
-            showStep(step + 2);
+        if (step == 0) {
             // Deselect product checked if there
             radioProd.filter(':checked').prop('checked', false);
+
+            showStep(step + 2);
         } else {
             showStep(step + 1);
         }
@@ -72,11 +57,76 @@ $(document).ready(function () {
 
 
 
-function showStep(nextStep) {
+jQuery.validator.addMethod('radiosRequired', function (value, element, options) {
+    // Page, where this method must work
+    var page = parseInt(options);
 
+    // Do not check if steps back
+    if (nextStep < step) {
+        return true;
+    }
+
+    // Do not check if another page
+    if (step != page) {
+        return true;
+    }
+
+    var len = radioProdType.filter(':checked').length;
+    return len > 0;
+}, 'Please select product type!');
+
+jQuery.validator.addMethod('inputRequired', function (value, element, options) {
+    // Page, where this method must work
+    var page = parseInt(options);
+
+    // Do not check if steps back
+    if (nextStep < step) {
+        return true;
+    }
+
+    if (step != page) {
+        return true;
+    }
+
+    // True - test passed, False - test not passed
+    return (value.trim()).length > 0;
+}, 'Please fill this field');
+
+form.validate({
+    // debug: true,
+    ignore: [], // to validate hidden inputs, etc my hidden radios
+
+    rules: {
+        productType: {
+            radiosRequired: 1, // step number (0-based) where this field required
+        },
+
+        inputStep3: {
+            inputRequired: 2, // step number (0-based)
+        },
+
+        inputStep4: {
+            inputRequired: 3, // step number (0-based)
+        },
+
+    },
+
+    errorPlacement: function (error, element) {
+        if (element.is(radioProdType)) {
+            // error.appendTo( element.parents('.container') );
+            alert(error[0].textContent);
+        } else { // This is the default behavior 
+            error.insertAfter(element);
+        }
+        
+    },
+
+});
+
+function showStep(next) {
+    nextStep = next;
     // Prevent form validation on PREV button pressed
-    if (form.valid() === false && (nextStep > step)) {
-        console.log('not valid form');
+    if (!form.valid()) {
         return false;
     }
 
@@ -85,14 +135,14 @@ function showStep(nextStep) {
 
     current_fs.hide();
     next_fs.show();
-        
+
     // Show/hide buttons
     if (nextStep === 0) {
         btnPrev.prop('disabled', true);
     } else {
         btnPrev.prop('disabled', false);
     }
-    
+
     if (nextStep === max_step) {
         btnNext.hide();
         btnSubmit.show();
@@ -100,31 +150,31 @@ function showStep(nextStep) {
         btnNext.show();
         btnSubmit.hide();
     }
-    
+
     // Progressbar
     var i = 0;
-    
+
     for (i = 0; i < nextStep; i++) {
         progressbar.eq(i).addClass('pass');
     }
-    
+
     for (i = nextStep; i < max_step; i++) {
         progressbar.eq(i).removeClass('pass');
     }
-    
+
     progressbar.eq(step).removeClass('active');
     progressbar.eq(nextStep).addClass('active');
-    
+
     // Right panel with <dl>
     if (nextStep > step) {
         dl.eq(step + 1).show();
     } else {
         dl.eq(step).hide();
     }
-    
+
     // Do not remeber last step if we go back to prevent loop
     // between steps
-    if (nextStep > step ) {
+    if (nextStep > step) {
         stepStack.push(step);
     }
     step = nextStep;
